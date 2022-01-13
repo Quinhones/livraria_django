eaa87c850c9f3a1beced3983699ac3f8f479e498
Quinhones <matheus-quinhones@hotmail.com>
2022-01-13 03:09:42 -0200

    htest

--- livraria/settings.py
+++ livraria/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,7 +108,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
